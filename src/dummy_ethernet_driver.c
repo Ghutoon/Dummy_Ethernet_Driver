c2e03bec46d5a0958c0f1f1acab342c7db4ace6a
Ghutoon2k <54318831+Ghutoon@users.noreply.github.com>
2023-04-28 16:52:42 +0530

    Update dummy_ethernet_driver.c

--- src/dummy_ethernet_driver.c
+++ src/dummy_ethernet_driver.c
@@ -8,34 +8,24 @@
 #include <linux/net_tstamp.h>
 #include <net/rtnetlink.h>
 #include <linux/u64_stats_sync.h>
+
+/* Protocol specific headers */
 #include <linux/ip.h>
 #include <linux/icmp.h>
+#define DUMMY_DRV_NAME "dummy_eth"
 
-#define DRV_NAME "dummy-eth"
+/* Exported APIs from dummy HW modeule */
 extern int asl_hw_tx(struct sk_buff *skb);
 extern int32_t asl_request_irq(bool mode, int32_t (*dummy_rx)(struct sk_buff *));
 
-static int dummy_open(struct net_device *dev)
+struct dummy_eth_priv
 {
-        printk(KERN_ERR "DETH: %s() - called\n", __func__);
-        /*Open Handler Function
-        Starts to accept buffer
-        */
-        netif_start_queue(dev);
-
-        return 0;
-}
-
-static int dummy_close(struct net_device *dev)
-{
-        printk(KERN_ERR "DETH: %s() - called\n", __func__);
-        netif_stop_queue(dev);
-
-        return 0;
-}
+        struct net_device *ndev;
+};
 
 int32_t dummy_eth_rx(struct sk_buff *skb)
 {
+        printk(KERN_INFO "Inside function %s \n", __func__);
         struct iphdr *iph = NULL;
         struct icmphdr *icmph = NULL;
         int32_t addr = 0;
@@ -49,17 +39,17 @@ int32_t dummy_eth_rx(struct sk_buff *skb)
 
         /* Mangle the packet to send ICMP/ping reply */
         iph = ip_hdr(skb);
-        if (iph && iph->protocol == IPPROTO_ICMP)
+        if (iph->protocol == IPPROTO_ICMP)
         {
                 __wsum csum = 0;
 
                 icmph = icmp_hdr(skb);
                 if (icmph == NULL)
                 {
-                        printk(KERN_ERR "DUMMY_ETHERNET_DRIVER_LOG: no such ICMP header\n");
+                        printk(KERN_ERR "DETH: no such ICMP header\n");
                         goto free;
                 }
-                print_hex_dump(KERN_ERR, "DUMMY_ETHERNET_DRIVER_LOG B: ", 0, 16, 1, skb->data, skb->len, 0);
+                print_hex_dump(KERN_ERR, "DETH B: ", 0, 16, 1, skb->data, skb->len, 0);
                 /* Alter MAC addresses */
                 memcpy(eth_addr, skb->data, ETH_ALEN);
                 memmove(skb->data, skb->data + ETH_ALEN, ETH_ALEN);
@@ -75,16 +65,16 @@ int32_t dummy_eth_rx(struct sk_buff *skb)
                 csum = csum_partial((u8 *)icmph, ntohs(iph->tot_len) - (iph->ihl * 4), csum);
                 icmph->checksum = csum_fold(csum);
 
-                print_hex_dump(KERN_ERR, "DUMMY_ETHERNET_DRIVER_LOG A: ", 0, 16, 1, skb->data, skb->len, 0);
+                print_hex_dump(KERN_ERR, "DETH A: ", 0, 16, 1, skb->data, skb->len, 0);
                 /* Pass frame up. XXX: need to enable hairpin, as same netdev? */
-                printk(KERN_ERR "DUMMY_ETHERNET_DRIVER_LOG: ping packet came pushing up\n");
+                printk(KERN_ERR "DETH: ping packet came pushing up\n");
                 /* Dev is same here no need to assign - skb->dev = dev; */
                 skb->protocol = eth_type_trans(skb, skb->dev);
                 netif_rx(skb);
         }
         else
         {
-                printk(KERN_ERR "DUMMY_ETHERNET_DRIVER_LOG: not a ping packet\n");
+                printk(KERN_ERR "DETH: not a ping packet\n");
                 goto free;
         }
 
@@ -95,63 +85,96 @@ free:
         return 0;
 }
 
-static netdev_tx_t dummy_xmit(struct sk_buff *skb, struct net_device *dev)
+static int dummy_eth_tx(struct sk_buff *skb, struct net_device *ndev)
 {
-        /* Call HW xmit function */
+        printk(KERN_INFO "Inside function %s \n", __func__);
+        // Handle transmitted packets (dummy implementation)
         if (asl_hw_tx(skb))
                 dev_kfree_skb(skb);
-
         return NETDEV_TX_OK;
 }
 
-static const struct net_device_ops dummy_netdev_ops = {
-    .ndo_start_xmit = dummy_xmit,
-    .ndo_validate_addr = eth_validate_addr,
-    .ndo_set_mac_address = eth_mac_addr,
-    .ndo_open = dummy_open,
-    .ndo_stop = dummy_close,
+static int dummy_eth_open(struct net_device *ndev)
+{
+        printk(KERN_INFO "Inside function %s \n", __func__);
+        // Device open handler (dummy implementation)
+        netif_start_queue(ndev);
+        return 0;
+}
+
+static int dummy_eth_stop(struct net_device *ndev)
+{
+        printk(KERN_INFO "Inside function %s \n", __func__);
+        // Device stop handler (dummy implementation)
+        netif_stop_queue(ndev);
+        return 0;
+}
+
+static const struct net_device_ops dummy_eth_ops = {
+    .ndo_open = dummy_eth_open,
+    .ndo_stop = dummy_eth_stop,
+    .ndo_start_xmit = dummy_eth_tx,
 };
 
-static void dummy_setup(struct net_device *dev)
+static void dummy_eth_init(struct net_device *ndev)
 {
-        printk(KERN_ERR "DUMMY_ETHERNET_DRIVER_LOG:  %s() - called\n", __func__);
-        ether_setup(dev);
+        printk(KERN_INFO "Inside function %s \n", __func__);
+        // Initialize the net_device structure (dummy implementation)
+        struct dummy_eth_priv *priv = netdev_priv(ndev);
+        ether_setup(ndev);
+        ndev->netdev_ops = &dummy_eth_ops;
+        priv->ndev = ndev;
 
-        /* Initialize the device structure. */
-        dev->netdev_ops = &dummy_netdev_ops;
-        dev->needs_free_netdev = true;
+        ndev->flags = IFF_NOARP;
 }
-struct net_device *ndev;
-static int __init dummy_ethernet_driver_module_init(void)
+
+static void dummy_eth_exit(struct net_device *ndev)
 {
-        struct net_device *ndev;
-        printk(KERN_INFO "DUMMY_ETHERNET_DRIVER_LOG : Inside function %s\n", __func__);
+        printk(KERN_INFO "Inside function %s \n", __func__);
+        // Cleanup the net_device structure (dummy implementation)
+        struct dummy_eth_priv *priv = netdev_priv(ndev);
+        netif_stop_queue(ndev);
+        free_netdev(ndev);
+}
 
-        ndev = netdev_alloc(0, DRV_NAME, NET_NAME_UNKNOWN, dummy_setup);
-        if (!ndev)
-                return -ENOMEM;
-        int err;
+static struct net_device *dummy_eth_netdev;
 
-        if (err = register_netdevice(ndev) < 0)
-                goto err;
-
-        if ((err = asl_request_irq(true, &dummy_eth_rx)))
+static int __init dummy_eth_init_module(void)
+{
+        printk(KERN_INFO "Inside function %s \n", __func__);
+        // Module initialization (dummy implementation)
+        dummy_eth_netdev = alloc_netdev(sizeof(struct dummy_eth_priv), DUMMY_DRV_NAME,
+                                        NET_NAME_UNKNOWN, dummy_eth_init);
+        if (!dummy_eth_netdev)
         {
-                return err;
+                return -ENOMEM;
         }
 
-err:
-        free_netdev(ndev);
+        if (register_netdev(dummy_eth_netdev))
+        {
+                free_netdev(dummy_eth_netdev);
+                return -ENODEV;
+        }
+
+        if ((asl_request_irq(true, &dummy_eth_rx)))
+        {
+                return -1;
+        }
+
         return 0;
 }
 
-static int __init dummy_ethernet_driver_module_exit(void)
+static void __exit dummy_eth_cleanup_module(void)
 {
-        printk(KERN_INFO "DUMMY_ETHERNET_DRIVER_LOG : Inside function %s\n", __func__);
-        unregister_netdev(ndev);
-        return 0;
+        // Module cleanup (dummy implementation)
+        printk(KERN_INFO "Inside function %s \n", __func__);
+        unregister_netdev(dummy_eth_netdev);
+        dummy_eth_exit(dummy_eth_netdev);
 }
 
-module_init(dummy_ethernet_driver_module_init);
-module_exit(dummy_ethernet_driver_module_exit);
+module_init(dummy_eth_init_module);
+module_exit(dummy_eth_cleanup_module);
+
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Niladri Chatterjee");
+MODULE_DESCRIPTION("Dummy Ethernet driver");
